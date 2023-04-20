4956ad64df4c13f52ed8b92eaee39da2bf4991de
Mikael Addae <mikael.addae@gmail.com>
2023-04-20 14:21:59 -0400

    seconds

--- BranchingExcercise/Program.cs
+++ BranchingExcercise/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("New Branch");
+            git remote add origin https://github.com/MikaelAddae/BranchingExcercise.git
+            git branch -M main
+            git push - u origin main
         }
     }
 }
