2e28fec824b5516ced0bcee9f131a720f6845561
Yiding Wang <yiding.wang82@gmail.com>
2023-04-21 08:41:02 -0400

    change

--- BranchingExcercise/Program.cs
+++ BranchingExcercise/Program.cs
@@ -7,14 +7,12 @@
             Console.WriteLine("Hello, World!");
 
             Console.WriteLine("New Branch");
-            git remote add origin https://github.com/MikaelAddae/BranchingExcercise.git
-            git branch -M main
-            git push - u origin main
+       
 
             Console.WriteLine("yiding wang");
             Console.WriteLine("how are you doing 123456789");
             Console.WriteLine("12365478963214778899655");
-
+            Console.WriteLine( "1233333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333");
         }
     }
 }
